4085213418f787c14fa2bb1ad9bf1684beb75b09
harishkumar <harishsanjay3104@gmail.com>
2024-07-31 02:48:10 -0700

    harish

---
+++ code.java
@@ -0,0 +1,19 @@
+public class code {
+    public static void main(String[] args) {
+        // Print a simple message
+        System.out.println("Hello, World!");
+
+        // Print a message without a newline
+        System.out.print("Hello, ");
+        System.out.print("World!");
+
+        // Print a formatted string
+        int number = 42;
+        System.out.printf("The number is %d%n", number);
+
+        // Print multiple lines using println
+        System.out.println("Line 1");
+        System.out.println("Line 2");
+        System.out.println("Line 3");
+    }
+}
